2133d0d17017ac48e04c18ac6a55a18633d90617
Masahiro <masahir0.negi4@gmail.com>
2025-10-15 12:30:14 +0100

    enable documents for multiple versions

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -43,3 +43,31 @@ exclude_patterns = []
 html_theme = "sphinx_rtd_theme"
 html_show_sourcelink = False
 html_static_path = ["_static"]
+
+import os
+
+import yaml
+
+# get the environment variable build_all_docs and pages_root
+build_all_docs = os.environ.get("build_all_docs")
+pages_root = os.environ.get("pages_root", "")
+
+# if not there, we dont call this
+if build_all_docs is not None:
+	# we get the current language and version
+	current_version = os.environ.get("current_version")
+
+	# we set the html_context wit current language and version
+	# and empty languages and versions for now
+	html_context = {
+		"current_version": current_version,
+		"versions": [],
+	}
+
+	# and loop over all other versions from our yaml file
+	# to set versions and languages
+	with open("versions.yaml") as yaml_file:
+		docs = yaml.safe_load(yaml_file)
+
+	for version, details in docs.items():
+		html_context["versions"].append([version, pages_root + "/" + version])
