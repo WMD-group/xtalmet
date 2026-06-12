d081a6a2d1e9beb228d0d5c87506e56c21ee956d
masahiro-negishi <masahir0.negi4@gmail.com>
2026-06-12 13:21:40 +0100

    update docs to v1.0.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,9 +14,9 @@ sys.path.insert(0, str(Path(__file__).resolve().parents[2]))
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = "xtalmet"
-copyright = "2025, Masahiro Negishi"
+copyright = "2026, Masahiro Negishi"
 author = "Masahiro Negishi"
-release = "1.0.0a4"
+release = "1.0.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
